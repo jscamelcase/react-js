2557141ae29781401c4a8f2dc56ef2eb0033bae4
Carl Paraskevas <carl.paraskevas@gmail.com>
2025-01-14 14:31:23 +0000

    updated props section in bookstore

--- react-js/bookstore/src/components/Book/Book.jsx
+++ react-js/bookstore/src/components/Book/Book.jsx
@@ -1,24 +1,10 @@
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
-    alt="Interesting Facts For Curious Minds'"
-  />
-);
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75", marginTop: "0.5rem" }}>
-    Jordan Moore
-  </h4>
-);
-
-function Book() {
+function Book({ img, title, author, children }) {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="book cover" />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      {children}
     </article>
   );
 }
--- react-js/bookstore/src/components/BookList/BookList.jsx
+++ react-js/bookstore/src/components/BookList/BookList.jsx
@@ -1,12 +1,30 @@
 import Book from "./../Book/Book.jsx";
 
 const BookList = () => {
+  const firstBook = {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg",
+  };
+  const secondBook = {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+  };
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      >
+        <p>Hello world</p>
+      </Book>
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
     </section>
   );
 };
